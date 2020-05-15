20bf46ba87e5107e069f940945af5c1e0faac286
Adam Bernot <adam@bernot.dev>
2020-05-15 19:48:42 +0000

    feat: replace text fields with color pickers

--- src/track/TrackConfiguration.tsx
+++ src/track/TrackConfiguration.tsx
@@ -1,4 +1,6 @@
 import React, { ChangeEvent } from 'react';
+import { SketchPicker, ColorResult } from 'react-color';
+
 import './TrackConfiguration.css';
 
 const TrackConfiguration: React.FC<any> = (props) => {
@@ -12,31 +14,42 @@ const TrackConfiguration: React.FC<any> = (props) => {
     let evenColor = styleRef.getPropertyValue('--even-color');
     let oddColor = styleRef.getPropertyValue('--odd-color');
     let lineColor = styleRef.getPropertyValue('--line-color');
-    const handleOddColorChange = (event: ChangeEvent<HTMLInputElement>) => {
-        document.documentElement.style.setProperty('--odd-color', event.target.value);
+    const handleOddColorChange = (color: ColorResult) => {
+        document.documentElement.style.setProperty('--odd-color', color.hex);
     };
-    const handleEvenColorChange = (event: ChangeEvent<HTMLInputElement>) => {
-        document.documentElement.style.setProperty('--even-color', event.target.value);
+    const handleEvenColorChange = (color: ColorResult) => {
+        document.documentElement.style.setProperty('--even-color', color.hex);
     }
-    const handleLineColorChange = (event: ChangeEvent<HTMLInputElement>) => {
-        document.documentElement.style.setProperty('--line-color', event.target.value);
+    const handleLineColorChange = (color: ColorResult) => {
+        document.documentElement.style.setProperty('--line-color', color.hex);
     }
     
     return (
         <form>
-            <label htmlFor="trackSize">
-                Track Size: 
-            </label>
-            <select id="trackSize" defaultValue={trackSize} onChange={handleTrackSizeChange}>
-                <option value="200m">200m</option>
-                <option value="400m">400m</option>
-            </select>
-            <label>Odd lane color: </label>
-            <input type="text" defaultValue={oddColor} onChange={handleOddColorChange} />
-            <label>Even lane color: </label>
-            <input type="text" defaultValue={evenColor} onChange={handleEvenColorChange} />
-            <label>Line color: </label>
-            <input type="text" defaultValue={lineColor} onChange={handleLineColorChange} />
+            <div className="track-size-selector">
+                <label htmlFor="trackSize">
+                    Track Size
+                </label>
+                <br />
+                <select id="trackSize" defaultValue={trackSize} onChange={handleTrackSizeChange}>
+                    <option value="200m">200m</option>
+                    <option value="400m">400m</option>
+                </select>
+            </div>
+            <div className="color-pickers">
+                <label>
+                    Odd lane color
+                    <SketchPicker color={oddColor} disableAlpha={true} onChange={handleOddColorChange} />
+                </label>
+                <label>
+                    Even lane color
+                    <SketchPicker color={evenColor} disableAlpha={true} onChange={handleEvenColorChange} />
+                </label>
+                <label>
+                    Line color
+                    <SketchPicker color={lineColor} disableAlpha={true} onChange={handleLineColorChange} />
+                </label>
+            </div>
         </form>
     );
 }
